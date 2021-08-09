4d172b9e075029c91b97189250e6f48421f9a68d
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-09 22:49:03 +0300

    search for mistakes

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,8 +10,8 @@ router.post('/signup', users.signup)
 router.post('/login', users.login)
 router.get('/logout', users.logout)
 
-const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { cb(null, tempDir) }, filename: (req, file, cb) => { null, file.originalname } })
+const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { cb(null, tempDir) }, filename: (req, file, cb) => cb(null, file.originalname)})
 const saveToTempMiddleware = multer({ storage: storageSettings })
-router.patch('/avatars', saveToTempMiddleware.single('avatar'), users.avatarsUpload)
+router.patch('/avatars', saveToTempMiddleware.single('avatars'), users.avatarsUpload)
 
 module.exports = router
