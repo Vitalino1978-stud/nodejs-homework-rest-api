f80e073bc3fc6ef59ba42e4137ac40d037515e6c
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-09 22:10:29 +0300

    uplolad via multer to Temp

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/public', express.static('public'));
+app.use('/temp', express.static('temp'));
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', usersRouter)
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const { users } = require('../../controllers')
+const multer = require('multer')
+const path = require('path')
+const tempDir = path.join(process.cwd(), "temp")
+
 router.post('/signup', users.signup)
 router.post('/login', users.login)
 router.get('/logout', users.logout)
-router.patch('/avatars', users.avatarsUpload)
+
+const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { cb(null, tempDir) }, filename: (req, file, cb) => { null, file.originalname } })
+const saveToTempMiddleware = multer({ storage: storageSettings })
+router.patch('/avatars', saveToTempMiddleware.single('avatar'), users.avatarsUpload)
+
 module.exports = router
