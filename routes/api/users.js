45d9297b38efaa7496a20831f8e9b9e399bc53e9
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-10 21:58:10 +0300

    continue try to fix mistakes

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,22 +14,31 @@ router.get('/logout', users.logout)
 
 const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { cb(null, tempDir) }, filename: (req, file, cb) => cb(null, file.originalname)})
 const saveToTempMiddleware = multer({ storage: storageSettings })
-const resizeAndUpload = async (req, res, next) => {
-	const { path } = req.file
-	try {
-		const token = await req.headers.authorization.split(" ")[1];
+const resizeAndUpload = (req, res, next) => {
+	const {path: tempDir } = req.file
+	// try {
+	// 	const token = await req.headers.authorization.split(" ")[1];
+	// const { JWT_SECRET_KEY } = process.env;
+  //   jwt.verify(token, JWT_SECRET_KEY);
+	// 	const user = await jwt.decode(token);
+	// 	const avatarDirPath = await path.join(process.cwd(), 'public/avatars')
+	// 	const oldPathToFile = tempDir;
+	// 	const newPathFile = `${avatarDirPath}/${user.id}.png`
+	// 	await fs.rename(oldPathToFile, newPathFile)
+	// 	res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
+	// } catch (error) {
+	// 	next(error)
+	// }
+	
+		const token = req.headers.authorization.split(" ")[1];
 	const { JWT_SECRET_KEY } = process.env;
     jwt.verify(token, JWT_SECRET_KEY);
-		const user = await jwt.decode(token);
-		const avatarDirPath = await path.join(process.cwd(), 'public/avatars')
-		const oldPathToFile = path;
+		const user = jwt.decode(token);
+		const avatarDirPath = path.join(process.cwd(), 'public/avatars')
+		const oldPathToFile = tempDir;
 		const newPathFile = `${avatarDirPath}/${user.id}.png`
-		await fs.rename(oldPathToFile, newPathFile)
-		res.json({path, message:'from resizeAndUpload', userId: user.id, data: {user}})
-	} catch (error) {
-		next(error)
-	}
-
+	  fs.rename(oldPathToFile, newPathFile)
+		res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
 }
 
 
