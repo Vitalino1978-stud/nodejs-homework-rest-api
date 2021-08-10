dc0edd09bb91c9db792026d87b96686923816911
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-10 22:14:50 +0300

    adds jimp

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,8 @@ const path = require('path')
 const tempDir = path.join(process.cwd(), "temp")
 const fs = require('fs/promises')
 const jwt = require('jsonwebtoken')
-console.log(tempDir)
+const jimp = require('jimp')
+
 
 router.post('/signup', users.signup)
 router.post('/login', users.login)
@@ -16,29 +17,36 @@ const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { c
 const saveToTempMiddleware = multer({ storage: storageSettings })
 const resizeAndUpload = (req, res, next) => {
 	const {path: tempDir } = req.file
-	// try {
-	// 	const token = await req.headers.authorization.split(" ")[1];
-	// const { JWT_SECRET_KEY } = process.env;
-  //   jwt.verify(token, JWT_SECRET_KEY);
-	// 	const user = await jwt.decode(token);
-	// 	const avatarDirPath = await path.join(process.cwd(), 'public/avatars')
-	// 	const oldPathToFile = tempDir;
-	// 	const newPathFile = `${avatarDirPath}/${user.id}.png`
-	// 	await fs.rename(oldPathToFile, newPathFile)
-	// 	res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
-	// } catch (error) {
-	// 	next(error)
-	// }
-	
+	try {
+		Jimp.read(req.file.path).then(image => {
+      image.resize(250, 250);
+			image.write(tempDir);
 		const token = req.headers.authorization.split(" ")[1];
-	const { JWT_SECRET_KEY } = process.env;
+	  const { JWT_SECRET_KEY } = process.env;
     jwt.verify(token, JWT_SECRET_KEY);
 		const user = jwt.decode(token);
 		const avatarDirPath = path.join(process.cwd(), 'public/avatars')
 		const oldPathToFile = tempDir;
 		const newPathFile = `${avatarDirPath}/${user.id}.png`
 	  fs.rename(oldPathToFile, newPathFile)
-		res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
+		}).catch(error => {
+	console.log(error message)
+		})
+		next()
+		// res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
+	} catch (error) {
+		next(error)
+	}
+	
+// 		const token = req.headers.authorization.split(" ")[1];
+// 	const { JWT_SECRET_KEY } = process.env;
+//     jwt.verify(token, JWT_SECRET_KEY);
+// 		const user = jwt.decode(token);
+// 		const avatarDirPath = path.join(process.cwd(), 'public/avatars')
+// 		const oldPathToFile = tempDir;
+// 		const newPathFile = `${avatarDirPath}/${user.id}.png`
+// 	  fs.rename(oldPathToFile, newPathFile)
+// 		res.json({tempDir, message:'from resizeAndUpload', userId: user.id, data: {user}})
 }
 
 
