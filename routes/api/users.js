81bbf8b7bd42102c837fd9c68a89e11c93f8aeeb
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-10 21:14:02 +0300

    adds middleware resizeAndUpload

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const { users } = require('../../controllers')
 const multer = require('multer')
 const path = require('path')
 const tempDir = path.join(process.cwd(), "temp")
+const fs = require('fs/promises')
 console.log(tempDir)
 
 router.post('/signup', users.signup)
@@ -12,6 +13,16 @@ router.get('/logout', users.logout)
 
 const storageSettings = multer.diskStorage({ destination: (req, file, cb) => { cb(null, tempDir) }, filename: (req, file, cb) => cb(null, file.originalname)})
 const saveToTempMiddleware = multer({ storage: storageSettings })
-router.patch('/avatars', saveToTempMiddleware.single('avatars'), users.avatarsUpload)
+const resizeAndUpload = (req, res, next) => {
+	const { patch } = req.file
+	try {
+		res.json({path, message:'from resizeAndUpload'})
+	} catch (error) {
+		next(error)
+	}
+}
+
+
+router.patch('/avatars', saveToTempMiddleware.single('avatars'), resizeAndUpload, users.avatarsUpload)
 
 module.exports = router
