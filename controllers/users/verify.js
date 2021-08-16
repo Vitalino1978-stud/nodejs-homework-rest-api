77dd6910c07bcb3783e36b1ce1fad3647007730f
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-16 23:24:37 +0300

    fixes one mistake

--- controllers/users/verify.js
+++ controllers/users/verify.js
@@ -5,8 +5,7 @@ const verify = async (req, res, next) => {
 	try {
 		const candidate = await User.findOne({ verifyCode: req.params.verificationCode })
 			if (!candidate) {
-			return
-			res.status(404).json({message: "verify code has been expired"})
+			return res.status(404).json({message: "verify code has been expired"})
 		}
 
 		await User.findByIdAndUpdate(candidate._id, { verifyCode: "", verify: true})
