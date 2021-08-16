cbac44852387b43046646edf8fd6df452afdd529
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-08-17 00:44:52 +0300

    alittle bit correction

--- controllers/users/reverify.js
+++ controllers/users/reverify.js
@@ -9,6 +9,7 @@ const reverify = async (req, res, next) => {
 			return
 		}
 		const candidate = await User.findOne({ email })
+		console.log(candidate.verifyCode);
 		if (candidate === true || candidate.verify === true) {
 		// if (candidate?.verify === true) {
 			 res.status(400).json({
@@ -18,11 +19,11 @@ const reverify = async (req, res, next) => {
 			 })
 			return
 		}
-		if (candidate === false || candidate.verify === false) {
+		if (candidate.verify === false) {
 			const mail = {
 			to: email,
 			subject: "please, verify your email",
-			text: `press next link http://localhost:3000/api/users/verify/${code} to verify your email`
+			text: `press next link https://hw06-email.herokuapp.com/api/users/verify/${candidate.verifyCode} to verify your email`
 		}
 		await sendCodeToEmail(mail)
 			res.status(200).json({message: "verification code has been send to your email succsesful"})
